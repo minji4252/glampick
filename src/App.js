5acd7cc4c07fb998e38b84c5b06a837a64977b07
minji4252 <alswl7216@naver.com>
2024-06-29 16:02:41 +0900

    docs: 글램핑 상세페이지 top 작업중

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
+import RoomDetail from "./임시/RoomDetail";
+import "../src/styles/reset.css";
+import "../src/styles/common.css";
+import "../src/styles/color";
 
 function App() {
-  return <>글램픽 glampick</>;
+  return (
+    <>
+      글램픽 glampick
+      <RoomDetail />
+    </>
+  );
 }
 
 export default App;
