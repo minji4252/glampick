baad264540c19ec7894ad8b812b945b2f2b30def
minji4252 <alswl7216@naver.com>
2024-07-01 17:31:13 +0900

    feat: mypage 탭추가

---
+++ src/pages/mypage/Favorite.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Categories from "../../components/mypage/Categories";
+
+const Favorite = () => {
+  return (
+    <div>
+      <Categories />
+      <div>Favorite</div>
+    </div>
+  );
+};
+
+export default Favorite;
