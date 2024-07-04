573886b90b0b7b0dd9bee3cb555c892ce079e2f0
jojosoosoo <hyeoneegirl@naver.com>
2024-07-04 18:26:34 +0900

    design: 퍼블리싱 작업 완료

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/header.css";
 import { Link, useLocation } from "react-router-dom";
+import headerUser from "../../images/icon/member-icon.png";
 import glampickLogo from "../../images/glampick_logo.png";
-import glampickLogoMain from "../../images/glampick_logo_white.png";
+
 const Header = () => {
+  const [isLogin, setIsLogin] = useState(false);
   const locationNow = useLocation();
   // 메인 페이지 (Root)에서 Header 임시 숨김
   if (locationNow.pathname === "/") return null;
@@ -20,11 +22,30 @@ const Header = () => {
           </Link>
         </div>
         <div className="header-nav">
-          <button className="main-login">
-            <Link to="/login">
-              <p>로그인/회원가입</p>
-            </Link>
-          </button>
+          {isLogin ? (
+            <>
+              <button className="header-logout">
+                <Link to="/">
+                  <p>로그아웃</p>
+                </Link>
+              </button>
+              <div className="header-user">
+                <Link to="/bookingdetail" className="header-user-nav">
+                  <img
+                    src={headerUser}
+                    alt="헤더 유저 아이콘"
+                    className="header-user-icon"
+                  />
+                </Link>
+              </div>
+            </>
+          ) : (
+            <button className="header-login">
+              <Link to="/login">
+                <p>로그인/회원가입</p>
+              </Link>
+            </button>
+          )}
         </div>
       </div>
     </header>
