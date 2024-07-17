0138c51239429155841927f8ccf720300b3fdca2
jojosoosoo <hyeoneegirl@naver.com>
2024-07-17 17:17:04 +0900

    feat: favorite 로딩, main 지역에 전국 추가, search 페이지 이미지 불러옴

--- src/components/common/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-// // 로그인 상태에서만 접근 가능한 페이지들
-// import { Navigate } from "react-router-dom";
-
-// function PrivateRoute({ isLogin, children }) {
-//   return isLogin ? children : <Navigate to="/login" />;
-// }
-
-// export default PrivateRoute;
