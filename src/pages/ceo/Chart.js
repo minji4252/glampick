6060d122cf04931a51f37192f25f1142816ee39f
minji4252 <alswl7216@naver.com>
2024-08-02 17:07:51 +0900

    design: 객실 등록 페이지 퍼블리싱 90% 완료. 분석 페이지 퍼블리싱 시작

--- src/pages/ceo/Chart.js
+++ src/pages/ceo/Chart.js
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import { colorSystem, size } from "../../styles/color";
 import CeoCategories from "../../components/ceo/CeoCategories";
+import { useState } from "react";
 
 const WrapStyle = styled.div`
   .inner {
@@ -28,19 +29,80 @@ const WrapStyle = styled.div`
       margin-top: 250px;
     }
   }
+`;
 
-  .임시 {
-    height: 1000px;
+const ListStyle = styled.div`
+  width: 100%;
+
+  > div {
+    display: flex;
+    gap: 20px;
+    margin-bottom: 20px;
+    justify-content: flex-start;
+  }
+
+  li {
+    max-width: 200px;
+    width: 100%;
+    height: 130px;
+    border: 1px solid ${colorSystem.g200};
+    font-weight: 600;
+    color: ${colorSystem.g900};
+    font-size: 1.1rem;
+    list-style: none;
+    border-radius: 12px;
+    text-align: center;
+    line-height: 130px;
+    cursor: pointer;
+  }
+
+  .focused {
+    background-color: ${colorSystem.ceo300};
+    color: ${colorSystem.white};
   }
 `;
 
+const ListContent = styled.div`
+  font-size: 2rem;
+  margin-bottom: 30vh;
+`;
+
 const Chart = () => {
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const menuArr = [
+    { name: "예약", content: "예약입니다" },
+    { name: "매출", content: "매출입니다" },
+    { name: "취소율", content: "취소율입니다" },
+  ];
+
+  const selectMenuHandler = index => {
+    setCurrentTab(index);
+  };
+
   return (
     <WrapStyle>
       <CeoCategories />
       <div className="inner">
         <h3>매장 분석</h3>
-        <div className="임시"></div>
+        <ListStyle>
+          <div>
+            {menuArr.map((ele, index) => {
+              return (
+                <li
+                  key={index}
+                  className={
+                    currentTab === index ? "submenu focused" : "submenu"
+                  }
+                  onClick={() => selectMenuHandler(index)}
+                >
+                  {ele.name}
+                </li>
+              );
+            })}
+          </div>
+        </ListStyle>
+        <ListContent>{menuArr[currentTab].content}</ListContent>
       </div>
     </WrapStyle>
   );
