066e75be988925a701a8af12d3b0dd8bf7daa0da
jojosoosoo <hyeoneegirl@naver.com>
2024-08-13 14:59:59 +0900

    feat: 지도 마커 생성, 지역에 따른 리스트 불러오기

--- src/components/common/MapModal.js
+++ src/components/common/MapModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IoClose } from "react-icons/io5";
 import {
   MapModalStyle,
@@ -9,9 +9,36 @@ import {
 } from "../../styles/MapModalStyle";
 import SearchMap from "../search/SearchMap";
 import MapCard from "../search/MapCard";
+import axios from "axios";
 
-const MapModal = ({ isOpen, onClose, searchData, center, region, region1 }) => {
+const MapModal = ({ isOpen, onClose, center, region1 }) => {
+  const [searchData, setSearchData] = useState([]);
+
+  // 지역 기준으로 리스트 받아옴
+  useEffect(() => {
+    if (isOpen && region1) {
+      axios
+        .get("/api/glamping/search/map", {
+          params: { region: region1 },
+        })
+        .then(response => {
+          const { mapList } = response.data;
+          setSearchData(mapList);
+        })
+        .catch(error => {
+          console.error("지역 기준으로 리스트 받아옴 오류: ", error);
+        });
+    }
+  }, [isOpen, region1]);
+
   if (!isOpen) return null;
+
+  // searchData에서 위도 경도 가격 가져오기
+  const markers = searchData.map(item => ({
+    lat: parseFloat(item.lat),
+    lng: parseFloat(item.lng),
+    price: item.price,
+  }));
 
   return (
     <MapModalStyle>
@@ -38,7 +65,7 @@ const MapModal = ({ isOpen, onClose, searchData, center, region, region1 }) => {
             ))}
           </LeftList>
           <RightMap>
-            <SearchMap center={center} region={region} region1={region1} />
+            <SearchMap center={center} region1={region1} markers={markers} />
           </RightMap>
         </MapContent>
       </SearchMapModal>
@@ -47,71 +74,3 @@ const MapModal = ({ isOpen, onClose, searchData, center, region, region1 }) => {
 };
 
 export default MapModal;
-
-// import React, { useEffect, useState } from "react";
-// import { IoClose } from "react-icons/io5";
-// import {
-//   MapModalStyle,
-//   SearchMapModal,
-//   MapContent,
-//   LeftList,
-//   RightMap,
-// } from "../../styles/MapModalStyle";
-// import SearchMap from "../search/SearchMap";
-// import MapCard from "../search/MapCard";
-// import axios from "axios";
-
-// const MapModal = ({ isOpen, onClose, center, region, region1 }) => {
-//   const [searchData, setSearchData] = useState([]);
-
-//   useEffect(() => {
-//     if (isOpen) {
-//       // API 호출
-//       axios
-//         .get("/api/glamping/search/map")
-//         .then(response => {
-//           const { mapList } = response.data;
-//           setSearchData(mapList);
-//         })
-//         .catch(error => {
-//           console.error("데이터를 가져오는 중 오류가 발생했습니다.", error);
-//         });
-//     }
-//   }, [isOpen]);
-
-//   if (!isOpen) return null;
-
-//   return (
-//     <MapModalStyle>
-//       <SearchMapModal>
-//         <button onClick={onClose} className="close-btn">
-//           <IoClose />
-//         </button>
-//         <MapContent>
-//           <LeftList>
-//             {searchData.map(item => (
-//               <MapCard
-//                 key={item.glampId}
-//                 glampId={item.glampId}
-//                 glampPic={item.glampPic}
-//                 glampName={item.glampName}
-//                 region={item.region}
-//                 starPoint={item.starPoint}
-//                 reviewCount={item.reviewCount}
-//                 price={item.price}
-//                 inDate={item.inDate}
-//                 outDate={item.outDate}
-//                 people={item.people}
-//               />
-//             ))}
-//           </LeftList>
-//           <RightMap>
-//             <SearchMap center={center} region={region} region1={region1} />
-//           </RightMap>
-//         </MapContent>
-//       </SearchMapModal>
-//     </MapModalStyle>
-//   );
-// };
-
-// export default MapModal;
